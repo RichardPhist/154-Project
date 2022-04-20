85577fe12782ab5dff32f5d515155f9117f21778
julietaMendez <jmendez1234@mail.fresnostate.edu>
2022-04-19 17:38:08 -0700

    Added variation but need to fix random removal

--- montyHall.py
+++ montyHall.py
@@ -19,65 +19,81 @@ def monty_hall(sim_nums, doors_input, swap_door, variation):
         car_door = random.randrange(0, doors_input)
         user_choice = random.randrange(0, doors_input)
         monty_choice = car_door
-        opened_doorsList = open_goat_door(user_choice, monty_choice, doors_input, variation)
+        opened_door_list = open_door(user_choice, monty_choice, doors_input, variation)
 
 #swap the user's choice to a new door
         if swap_door == True:
-            new_user_choice = swap_user_choice(user_choice, opened_doorsList, doors_input)
+            new_user_choice = swap_user_choice(user_choice, opened_door_list, doors_input)
 
 #Possible game scenarios 
         if not swap_door and user_choice == car_door:
             no_swap_wins +=1
-            #print("User chose door#", user_choice, "\tmonty opened goat doors#", opened_doorsList, "\tcar door#", car_door, "\tWINNER!")
+            #print("User chose door#", user_choice, "\tmonty opened goat doors#", opened_door_list, "\tcar door#", car_door, "\tWINNER!")
         
         elif not swap_door and user_choice != car_door:
             no_swap_losses +=1
-            #print("User chose door#", user_choice, "\tmonty opened goat doors#", opened_doorsList,"\tcar door#", car_door )
+            #print("User chose door#", user_choice, "\tmonty opened goat doors#", opened_door_list,"\tcar door#", car_door )
 
         elif swap_door and new_user_choice == car_door:
             swap_wins +=1
-            #print("User chose door#",user_choice, "\tmonty opened goat doors#", opened_doorsList, "swapped to doors#", new_user_choice, "\tcar door#", car_door, "\tWINNER!")
+            #print("User chose door#",user_choice, "\tmonty opened goat doors#", opened_door_list, "swapped to doors#", new_user_choice, "\tcar door#", car_door, "\tWINNER!")
 
         elif swap_door and new_user_choice != car_door:
             swap_losses +=1
-            #print("User chose door#", user_choice,"\tmonty opened goat doors#", opened_doorsList,  "swapped to doors#", new_user_choice,"\tcar door#", car_door )
+            #print("User chose door#", user_choice,"\tmonty opened goat doors#", opened_door_list,  "swapped to doors#", new_user_choice,"\tcar door#", car_door )
                     
         else: print("error")
 
     return   no_swap_wins, swap_wins, swap_losses, no_swap_losses, sim_nums
 
 #open a goat door
-def open_goat_door(user_choice, monty_choice, doorsNum, variation):
-    open_goat_doorList=[]
+def open_door(user_choice, monty_choice, doorsNum, variation):
+    opened_door_lists=[]
     
     #add all doors to the list, remove the user's door and the winning door
     for i in range(0, doorsNum):
-        open_goat_doorList.append(i)
+        opened_door_lists.append(i)
 
-    #if the user happens to choose the winning door, one goat door must be left closed for them to swap to
-    if (user_choice == monty_choice):
-        open_goat_doorList.remove(user_choice)
-        open_goat_doorList.remove(random.randrange(len(open_goat_doorList))) 
-    else:
-        open_goat_doorList.remove(user_choice)
-        open_goat_doorList.remove(monty_choice)
+    #If the variation is not chosen, the host may only open a goat door
+    if(variation == False):
+
+        #if the user chooses the winning door, the host can choose from either leftover door
+        #so leave a random door for the host to open
+        if (user_choice == monty_choice):
+            opened_door_lists.remove(user_choice) #remove the users choice from the hosts list
+            opened_door_lists.remove(random.randrange(len(opened_door_lists))) #remove a random goat door
+
+        #The only goat door the host can open is the door the user did not choose 
+        #and the one that does not have a car behind it 
+        else:
+            opened_door_lists.remove(user_choice) #remove the users choice from the hosts list
+            opened_door_lists.remove(monty_choice) #remove the car door from the hosts list
     
-    return open_goat_doorList
+    #else - the variation is  chosen: the host may open any door by accident
+    #minus two leftover doors 
+    else:
+        opened_door_lists.remove(random.randrange(len(opened_door_lists))) #remove a random door
+        #opened_door_lists.remove(random.randrange(len(opened_door_lists))) #remove a random door
+
+    return opened_door_lists
    
 
 #allows user to swap their choice after the host opens goat_doors. New door can't be one that is already open nor the user's current choice
-def swap_user_choice(user_choice, opened_doorsList, doorsNum ):
+def swap_user_choice(user_choice, opened_door_list, doorsNum):
     new_user_door = random.randrange(0, doorsNum)
-    while(new_user_door in opened_doorsList or new_user_door == user_choice):
+    while(new_user_door in opened_door_list or new_user_door == user_choice):
         new_user_door = random.randrange(0, doorsNum)
     return new_user_door
 
 
 
-tests = 1000
+tests = 3
 doors = 3
-sim_swap_results = monty_hall(tests, doors, True) 
-sim_no_swap_results = monty_hall(tests, doors, False)
+variation = True #change to run with or without variation
+
+sim_swap_results = monty_hall(tests, doors, True, variation) 
+sim_no_swap_results = monty_hall(tests, doors, False, variation)
+
 print("\n---------------------------- Monty Hall Simulation Results ----------------------------")
 print("Number of tests: ", tests)
 print("Number of doors: ", doors)
@@ -90,7 +106,13 @@ count=0
 
 for i in range(1, tests):
     testsList.append(i)
-    y = monty_hall(i, doors, True)
+
+    #run monty hall simulation  with i as the count of simulations,
+    #doors as the count of doors, 
+    #True/False as switch/no switch,
+    #and True/False as variatoin/no variation
+
+    y = monty_hall(i, doors, True, variation) 
     percent = (y[1]/y[4]*100)
     winList.append (percent)
     count += percent
